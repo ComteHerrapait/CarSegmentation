c2a52c279b781eb9d287d30dcb5d3db37a17df3e
Arthur Boiron <41791422+Mousquitaire@users.noreply.github.com>
2020-01-26 18:15:56 +0100

    Ajout de la cartoonisation dans preTraitement

--- PreTraitement.m
+++ PreTraitement.m
@@ -2,8 +2,9 @@ function [ ImgFin ] = PreTraitement( ImgInit, showSteps )
 % * Fonction de pre-traitement de l'image
 % * Prend une mage uint8 en entré et renvoie une image binarise
 
-ImgGray = rgb2gray(ImgInit); % Image uint8 => Image niveau de gris 
-ImgAdapt = adapthisteq(ImgGray); % ???
+ImgGray = rgb2gray(ImgInit); % Image uint8 => Image niveau de gris
+ImgCartoon = cartoon(ImgGray);
+ImgAdapt = adapthisteq(ImgCartoon); % ???
 ImgThresh = adaptthresh(ImgAdapt); % Calcul d'un seuil adaptatif
 ImgFin = imbinarize(ImgAdapt, ImgThresh); % Image cartoonise => Image binaire
 
