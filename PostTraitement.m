cf9cf08cbc7c4000fb1254ab581d19ae7cca0968
ComteHerrapait <leondecheux@gmail.com>
2020-01-26 17:56:38 +0100

    fixed successrate test

--- PostTraitement.m
+++ PostTraitement.m
@@ -1,17 +1,20 @@
-function [Contours] = PostTraitement( ImgTraitement, ImgInit )
+function [nCarsDetected] = PostTraitement( ImgTraitement, ImgInit , showSteps)
 % * Fonction de post-traitement
 % * Prend en argument l'image initiale et l'image renvoyee par le traitement
 
 Contours = regionprops(ImgTraitement, 'BoundingBox'); % Contours des objets de l'image
-imshow(ImgInit, []);
-nbVoiture = 0;
-for k = 1:length(Contours) % Iteration sur tous les contours
-    thisBB = Contours(k).BoundingBox;
-    if (thisBB(4)>thisBB(3)*1.5 && thisBB(4)<thisBB(3)*3) 
-        rectangle('Position',[thisBB(1),thisBB(2),thisBB(3),thisBB(4)],'EdgeColor','b','LineWidth',5 );
-        nbVoiture = nbVoiture + 1;
+if showSteps
+    imshow(ImgInit, []);
+    nbVoiture = 0;
+    for k = 1:length(Contours) % Iteration sur tous les contours
+        thisBB = Contours(k).BoundingBox;
+        if (thisBB(4)>thisBB(3)*1.5 && thisBB(4)<thisBB(3)*3) 
+            rectangle('Position',[thisBB(1),thisBB(2),thisBB(3),thisBB(4)],'EdgeColor','b','LineWidth',5 );
+            nbVoiture = nbVoiture + 1;
+        end
     end
-end
-title(sprintf('Il y a %.0f voitures', nbVoiture));
+    title(sprintf('Il y a %.0f voitures', nbVoiture));
+end 
+nCarsDetected = length(Contours);
 end
 
