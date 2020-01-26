1a1ae0fa1d8a8a90da63bc623a283ec33ad71e7b
Arthur Boiron <41791422+Mousquitaire@users.noreply.github.com>
2020-01-26 18:16:45 +0100

    Ajout de commentaires

--- PreTraitement.m
+++ PreTraitement.m
@@ -3,7 +3,7 @@ function [ ImgFin ] = PreTraitement( ImgInit, showSteps )
 % * Prend une mage uint8 en entré et renvoie une image binarise
 
 ImgGray = rgb2gray(ImgInit); % Image uint8 => Image niveau de gris
-ImgCartoon = cartoon(ImgGray);
+ImgCartoon = cartoon(ImgGray); % Cartoonisation de l'image
 ImgAdapt = adapthisteq(ImgCartoon); % ???
 ImgThresh = adaptthresh(ImgAdapt); % Calcul d'un seuil adaptatif
 ImgFin = imbinarize(ImgAdapt, ImgThresh); % Image cartoonise => Image binaire
