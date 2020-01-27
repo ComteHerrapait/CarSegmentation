3d16085711c816085093548f497c70c1ac446e59
ComteHerrapait <leondecheux@gmail.com>
2020-01-27 20:16:09 +0100

    new success rate test

--- SuccessRateTest.m
+++ SuccessRateTest.m
@@ -1,29 +1,46 @@
 %% Program to test the success rate of the current algorithm
 
+%%  Initialisation
 nTested = 0;
 nValid = 0;
 
 for i=1:30
-    name = num2str(i,'%03.f');%mise en forme du nom de l'image
-    path = strcat('Images/', name,'.jpg');%chemin vers l'image
-    image = imread(path);
+    %% Chemin d'accès
+    name = num2str(i,'%03.f');              %mise en forme du nom de l'image
+    path = strcat('Images/', name,'.jpg');  %chemin vers l'image
     
-    file = fopen(strcat('Images/Annotations/',name,'.annotation.txt'));
-    if file ~= -1 %si le fichier s'ouvre bien :
-        line = fgetl(file); %premiere ligne du texte
-        nCarsTheoric = str2num(line); %conversion en nombre
-        nTested = nTested + 1;
+    %% Ouverture des fichiers
+    image = imread(path);                                              	%ouverture de l'image
+    file = fopen(strcat('Images/Annotations/',name,'.annotation.txt'));	%ouverture de l'annotation
+    
+    %% Test de l'ouverture
+    if (file ~= -1)&&(~isempty(image))
+        nTested = nTested + 1;%compteur d'images traitées
         
+        %%Lecture des annotations
+        line = fgetl(file);                 %deuxième ligne du texte d'annotations
+        nCarsTheoricMin = str2num(line);	%conversion en nombre
+        line = fgetl(file);                 %premiere ligne du texte d'annotations
+        nCarsTheoricMax = str2num(line);    %conversion en nombre
+    
+        %%Traitement de l'image
         ImgPreTrait = PreTraitement(image, 0);
         ImgTraitement = Traitement(ImgPreTrait, 0);
         nCarsDetected = PostTraitement(ImgTraitement, image, 0);
         
-        if nCarsDetected == nCarsTheoric
+        %%Affichage en cours de traitement
+        fprintf('num = %.0f \tmin = %.0f \tmax = %.0f \ttrouvées = %.0f\n',i, nCarsTheoricMin, nCarsTheoricMax, nCarsDetected)
+        
+        %%teste si les valeurs sont correctes et si les valeurs du fichier
+        %%d'annotation existent
+        if ~isempty(nCarsTheoricMin) && ~isempty(nCarsTheoricMax) && (nCarsDetected >= nCarsTheoricMin) && (nCarsDetected <= nCarsTheoricMax)
             nValid = nValid + 1;
         end
         
+        %%Fermeture du fichier
         fclose(file);
     end
-
 end
-successRate = nValid / nTested
+
+%% Affichage du résultat
+fprintf('\n\nTaux de réussite au traitement : %.1f %%\n\n',100 * nValid / nTested);
